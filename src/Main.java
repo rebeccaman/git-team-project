efd3772e2cb5596f0044649d3a750de0a1972687
Neha Dayma <neha.dayma@hm.com>
2021-04-12 11:54:09 +0200

    Addition method and Menu

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
-    // Add code
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Please enter the second number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println();
+        System.out.println("What would you like to do:");
+        System.out.println("1 for Addition");
+        System.out.println("2 for Subtraction");
+        System.out.println("3 for Multiplication");
+        int userInput = scanner.nextInt();
+        System.out.println();
+
+        if (userInput == 1){
+            System.out.println("Addition result:" + addition(num1,num2));
+        }
+    }
+
+    public static int addition(int a, int b){
+        return a+b;
+    }
 }
